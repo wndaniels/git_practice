37c858f4a1a053c7111770b8442c1d34e9f40254
Nathan Daniels <85182159+wndaniels@users.noreply.github.com>
2021-07-20 18:27:45 -0400

    js added

---
+++ script.js
@@ -0,0 +1,9 @@
+(function() {
+	const searchBar = document.getElementById('search-bar');
+	const searchBtn = document.getElementById('search-btn');
+	const newSearch = searchBar.value;
+
+	searchBtn.addEventListener('submit', function() {
+		window.open('https://www.google.com/search?q' + newSearch);
+	});
+})();
